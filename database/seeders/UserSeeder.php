363752c8d12ed5a681382dafa4bec86d3c767cb1
rizalgemilangputra <rizal.gemilangputra@gmail.com>
2022-11-24 06:54:35 +0700

    fix UserSeeder & PupukSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,19 +15,24 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $time = date('Y-m-d');
         $users = [
             [
                 'email'    => 'rizal@gmail.com',
                 'password' => Hash::make('rizal@2022'),
-                'token'    => 't5HyLfctpAwNhplxUdq01K3UI9BWct2boRAXqvU3EKrCLVeAWPrydrvfidrhQRvfVS5GHe2U8CQSYC0Q'
+                'token'    => 't5HyLfctpAwNhplxUdq01K3UI9BWct2boRAXqvU3EKrCLVeAWPrydrvfidrhQRvfVS5GHe2U8CQSYC0Q',
+                'created_at'    => $time,
+                'updated_at'    => $time,
             ],
             [
                 'email'    => 'yohanes@gmail.com',
                 'password' => Hash::make('yohanes@2022'),
-                'token'    => 't5HyLfctpAwNhplxUdq01K3UI9BWct2boRAXqvU3EKrCLVeAWPrydrvfidrhQRvfVS5GHe2U8CQSYzzz'
+                'token'    => 't5HyLfctpAwNhplxUdq01K3UI9BWct2boRAXqvU3EKrCLVeAWPrydrvfidrhQRvfVS5GHe2U8CQSYzzz',
+                'created_at'    => $time,
+                'updated_at'    => $time,
             ],
         ];
 
-        User::create($users);
+        User::insert($users);
     }
 }
